3574b2adf2d47b998add84ecdc51189db394835c
vasov97 <vasovmail@gmail.com>
2024-11-03 22:34:16 +0100

    BankUI

---
+++ app/src/main/java/com/example/bankui/data/Card.kt
@@ -0,0 +1,11 @@
+package com.example.bankui.data
+
+import androidx.compose.ui.graphics.Brush
+
+data class Card(
+    val cardType: String,
+    val cardNumber: String,
+    val cardName: String,
+    val balance: Double,
+    val color: Brush
+)
